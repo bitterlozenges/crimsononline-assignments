c25b80ba5fe3dcd53d038856f1a381c9c7bb89fc
Tiffany Wu <tiffanywu@Tiffanys-MacBook-Pro.local>
2013-10-09 20:32:16 -0400

    ASSIGNMENT 2 COMPLETE

--- assignment2/question1.py
+++ assignment2/question1.py
@@ -34,9 +34,11 @@ class Content(object):
         print '{0}:{1}\n{2}'.format(self.title,self.subtitle, self.content)
 
 
-    def matches_url():
+    def matches_url(url):
 
-        pass
+        pattern = r'http://thecrimson.com/(\w+)/(\d{4})/(\d{1,2})/(\d{1,2})/([\w-]+)/'
+
+        return re.search(pattern, url)
 
 
 
@@ -49,7 +51,7 @@ class Article(Content):
         - All methods of Content
     '''
 
-    def __init__(self, title, subtitle, creator, publication_date, related_image= NONE):
+    def __init__(self, title, subtitle, creator, publication_date, related_image= None):
 
         super(Article,self).__init__(self, title, subtitle, creator, publication_date)
 
@@ -91,7 +93,54 @@ class Picture(Content):
 Question 1e
 '''
 def from_url(c_lst, url):
-    pass
+
+    pattern = r'http://thecrimson.com/(\w+)/(\d{4})/(\d{1,2})/(\d{1,2})/([\w-]+)/'
+
+    Type = None
+
+    year = None
+
+    month = None
+
+    year = None
+
+    Slug = None
+
+    match = re.search(pattern, url)
+
+    if match:
+
+        Type = match.groups()[0]
+
+        year = int(match.groups()[1])
+
+        month = int(match.groups()[2])
+
+        day = int(match.groups()[3])
+
+        Slug = match.groups()[4]
+
+        sl = Slug.split('-')
+
+        Slug = ''
+
+        for (s in sl):
+
+            Slug = Slug + s + ' '
+        Slug = Slug[:a.__len__()-1]  #here the slug is equal to a title
+
+
+    for (c in c_list):
+
+        Date = c.publication_date
+
+        if type(c) == Type && year == Date.year && month = Date.month && day = Date.day:
+
+            return c
+
+        return None
+        
+    
 
 '''
 Question 1e
